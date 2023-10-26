ec2030c91f87705e814d2fc57d7c2756e5657845
bmbo123 <gregoryalan2005@gmail.com>
2023-10-26 00:11:01 -0500

    worked on the frontend

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,7 @@ import "tailwindcss/tailwind.css";
 function Home() {
   return (
     <div className="home-main">
-      <div className="h-200 bg-gradient-to-b from-indigo-700 to-indigo-800 rounded-custom ">
+      <div className="h-200 bg-gradient-to-b from-indigo-700 to-indigo-800 rounded-custom top-20 ">
         <div className="title-box">
           <h1 className="title">interviewgpt</h1>
         </div>
